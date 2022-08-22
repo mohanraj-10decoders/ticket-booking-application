4a7f76b3ad777863afe60a2b62413e0958e2583e
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-08-22 12:34:35 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -17,7 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        Ticket booking application
       </header>
     </div>
   );
